8edbe83f9d7cbd4a5a575ddd49f47b176e5fb339
dizhentai <81971401@qq.com>
2020-05-20 15:42:13 +0800

    /添加的是同城的 appid

--- siteinfo.js
+++ siteinfo.js
@@ -1,9 +1,10 @@
 module . exports = {
-                name: "贺州美牙",
-                uniacid: "25",
-                acid: "25",
+                name: "播浪美业",
+                uniacid: "8",
+                acid: "8",
                 multiid:"0",
                 version: "1.2",
-                siteroot: "https://xcx.hrtdxcx.com/app/index.php",
+                siteroot:
+"https://haochtech.com/ys_api/app/index.php",
                 design_method: "3"
             };
